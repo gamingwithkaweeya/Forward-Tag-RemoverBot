63bc6dae9e1ac9d8942cac23702eecd7f6d5ea9f
KAJ <72274248+kmacprt@users.noreply.github.com>
2021-02-02 02:10:41 -0800

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ import os
 
 class Config:
 	
- TOKEN="1646853476:AAEb-xdgP6x0ZHyPCVg2-nsTnCvF6gh7"
+ TOKEN=os.environ.get("BOT_TOKEN")
  START_TEXT="Hi [{}](tg://user?id={})\nI am A Forward Tag remover Bot.Send /help To Know What I Can Do \n ©kavinduaj"
  HELP_TEXT="Forward Me A File,Video,Audio,Photo or Anything And \nI will Send You the File Back\n\n`How to Set Caption?`\nReply Caption to a File,Photo,Audio,Media"
 	
