7861b5ba2e35b3b92df2a25044396f22e6e76167
KAJ <72274248+kmacprt@users.noreply.github.com>
2021-02-02 01:27:46 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # ForwardTagRemoverBot - A Telegram Bot To Hide Forward Source
-#Copyright (C) 2020 by Rasak <https://github.com/Artis7eeR>
+#Copyright (C) 2020 by Rasak <https://github.com/Artis7eeR> <https://github.com/kmacprt>
 #ForwardTagRemoverBot is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
 #the Free Software Foundation, either version 3 of the License, or
@@ -19,7 +19,6 @@ import os
 class Config:
 	
  TOKEN=os.environ.get("BOT_TOKEN",None)
- SOURCE="https://github.com/Artis7eeR/ForwardTagRemoverBot"
- START_TEXT="Hi [{}](tg://user?id={})\nI am A Forward Tag remover Bot.Send /help To Know What I Can Do \n ©Artis7eeR"
+ START_TEXT="Hi [{}](tg://user?id={})\nI am A Forward Tag remover Bot.Send /help To Know What I Can Do \n ©kavinduaj"
  HELP_TEXT="Forward Me A File,Video,Audio,Photo or Anything And \nI will Send You the File Back\n\n`How to Set Caption?`\nReply Caption to a File,Photo,Audio,Media"
 	
